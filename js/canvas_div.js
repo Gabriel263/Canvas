9d80dace073f33b53399b4dfedfdb1c54238971f
Gabriel Blum <63974834+Gabriel263@users.noreply.github.com>
2021-09-24 12:45:43 -0400

    Canvas comentado

--- js/canvas_div.js
+++ js/canvas_div.js
@@ -1,78 +1,111 @@
+//Contadores que são utilizados para especificação de id dinâmicos para que cada div seja única 
 let id_c1 = 0;
 let id_c2 = 0;
 let id_c3 = 0;
 let id_c4 = 0;
-async function newdiv1(){
-        document.getElementById('pop_up1').style.display = "none";
-        var id_fonte = "choises_fon1";
-        var id_fundo = "choises_fun1"; 
-        var id_canvas = "col1";
-        var id_titulo = "tittle1";
-        var id_textarea = "areatexto1";
-        var id_cabecalho = "cabecalho"; 
-        var areatexto = await document.getElementById('areatexto1').value;
-        
-        await div(id_fundo, id_canvas, id_c1.toString());
-        
-        await trash(id_canvas,id_cabecalho, id_c1.toString());
-        await titulo(id_fonte, id_fundo, id_titulo, id_cabecalho, id_c1.toString());
+//Contadores que são utilizados para especificação de id dinâmicos para que cada div seja única
 
-        await br(id_canvas, id_c1.toString());
-        await texto1(id_fonte, id_fundo, id_canvas, areatexto, id_c1.toString());
-        await limpar(id_titulo, id_textarea, id_fonte, id_fundo);
-        id_c1= somaID(id_c1);
+//Gera as divs do primeiro canvas, utiliza mudança do valor da variáveis a cada clique
+async function newdiv1(){
+  
+  //Define qual serão os ids conforme o elemento clicado, captura o que foi digitado no campo descrição e abre a pop up para digitação
+  document.getElementById('pop_up1').style.display = "none";
+  var id_fonte = "choises_fon1";
+  var id_fundo = "choises_fun1"; 
+  var id_canvas = "col1";
+  var id_titulo = "tittle1";
+  var id_textarea = "areatexto1";
+  var id_cabecalho = "cabecalho1"; 
+  var areatexto = await document.getElementById('areatexto1').value;
+  //Define qual serão os ids conforme o elemento clicado, captura o que foi digitado no campo descrição e abre a pop up para digitação
+
+
+  //Realiza a comunição com as variáveis definidas após o click com as funções de criação das divs, e manter contadores para cada um dos canvas
+  await div(id_fundo,id_cabecalho,id_canvas,id_c1.toString());
+  await trash(id_canvas,id_cabecalho, id_c1.toString());
+  await titulo(id_fonte, id_fundo, id_titulo,id_canvas, id_cabecalho, id_c1.toString());
+  await br(id_canvas, id_c1.toString());
+  await texto1(id_fonte, id_fundo, id_canvas, areatexto, id_c1.toString());
+  await limpar(id_titulo, id_textarea, id_fonte, id_fundo);
+  id_c1= somaID(id_c1);
+  //Realiza a comunição com as variáveis definidas após o click com as funções de criação das divs, e manter contadores para cada um dos canvas
 }
+//Gera as divs do primeiro canvas, utiliza mudança do valor da variáveis a cada clique
+
+//Gera as divs da segunda canvas, utiliza mudança do valor da variáveis a cada clique
 async function newdiv2(){
+
+  //Define qual serão os ids conforme o elemento clicado, captura o que foi digitado no campo descrição e abre a pop up para digitação
   document.getElementById('pop_up2').style.display = "none";
   var id_fonte = "choises_fon2";
   var id_fundo = "choises_fun2"; 
   var id_canvas = "col2";
   var id_titulo = "tittle2";
-  var id_textarea = "areatexto2"; 
-  var areatexto = await document.getElementById('areatexto1').value;
-  // await teste("Foi");
-  await div(id_fundo, id_canvas, id_c2.toString());
-  await div2();
-  await trash(id_canvas, id_c2);
-  await titulo(id_fonte, id_fundo, id_titulo, id_canvas, id_c2.toString());
+  var id_textarea = "areatexto2";
+  var id_cabecalho = "cabecalho2"; 
+  var areatexto = await document.getElementById('areatexto2').value;
+  //Define qual serão os ids conforme o elemento clicado, captura o que foi digitado no campo descrição e abre a pop up para digitação
+
+  //Realiza a comunição com as variáveis definidas após o click com as funções de criação das divs, e manter contadores para cada um dos canvas
+  await div(id_fundo,id_cabecalho, id_canvas, id_c2.toString());
+  await trash(id_canvas,id_cabecalho, id_c2.toString());
+  await titulo(id_fonte, id_fundo, id_titulo,id_canvas, id_cabecalho, id_c2.toString());
   await br(id_canvas, id_c2.toString());
   await texto1(id_fonte, id_fundo, id_canvas, areatexto, id_c2.toString());
   await limpar(id_titulo, id_textarea, id_fonte, id_fundo);
   id_c2= somaID(id_c2);
+  //Realiza a comunição com as variáveis definidas após o click com as funções de criação das divs, e manter contadores para cada um dos canvas
 }
+//Gera as divs da segunda canvas, utiliza mudança do valor da variáveis a cada clique
+
+//Gera as divs da tereira canvas, utiliza mudança do valor da variáveis a cada clique
 async function newdiv3(){
+
+  //Define qual serão os ids conforme o elemento clicado, captura o que foi digitado no campo descrição e abre a pop up para digitação
   document.getElementById('pop_up3').style.display = "none";
   var id_fonte = "choises_fon3";
   var id_fundo = "choises_fun3"; 
   var id_canvas = "col3";
   var id_titulo = "tittle3";
-  var id_textarea = "areatexto3"; 
+  var id_textarea = "areatexto3";
+  var id_cabecalho = "cabecalho3";  
   var areatexto = await document.getElementById('areatexto3').value;
-  // await teste("Foi");
-  await div(id_fundo, id_canvas, id_c3.toString());
-  await div2();
-  await trash(id_canvas, id_c3);
-  await titulo(id_fonte, id_fundo, id_titulo, id_canvas, id_c3.toString());
+  //Define qual serão os ids conforme o elemento clicado, captura o que foi digitado no campo descrição  e abre a pop up para digitação
+
+  //Realiza a comunição com as variáveis definidas após o click com as funções de criação das divs, e manter contadores para cada um dos canvas
+  await div(id_fundo,id_cabecalho, id_canvas, id_c3.toString());
+  await trash(id_canvas,id_cabecalho, id_c3.toString());
+  await titulo(id_fonte, id_fundo, id_titulo,id_canvas, id_cabecalho, id_c3.toString());
   await br(id_canvas, id_c3.toString());
   await texto1(id_fonte, id_fundo, id_canvas, areatexto, id_c3.toString());
   await limpar(id_titulo, id_textarea, id_fonte, id_fundo);
   id_c3= somaID(id_c3);
+  //Realiza a comunição com as variáveis definidas após o click com as funções de criação das divs, e manter contadores para cada um dos canvas
 }
+//Gera as divs da terceira canvas, utiliza mudança do valor da variáveis a cada clique
+
+//Gera as divs da quarta canvas, utiliza mudança do valor da variáveis a cada clique
 async function newdiv4(){
+
+  //Define qual serão os ids conforme o elemento clicado, captura o que foi digitado no campo descrição e abre a pop up para digitação
   document.getElementById('pop_up4').style.display = "none";
   var id_fonte = "choises_fon4";
   var id_fundo = "choises_fun4";
   var id_canvas = "col4";
   var id_titulo = "tittle4";
-  var id_textarea = "areatexto4"; 
+  var id_textarea = "areatexto4";
+  var id_cabecalho = "cabecalho4";  
   var areatexto = await document.getElementById('areatexto4').value;
-  // await teste("Foi");
-  await div(id_fundo, id_canvas, id_c4.toString());
-  await div2();
-  await trash(id_canvas, id_c4);
-  await titulo(id_fonte, id_fundo, id_titulo, id_canvas, id_c4.toString());
+  //Define qual serão os ids conforme o elemento clicado, captura o que foi digitado no campo descrição e abre a pop up para digitação
+  
+  //Realiza a comunição com as variáveis definidas após o click com as funções de criação das divs, e manter contadores para cada um dos canvas
+  await div(id_fundo,id_cabecalho, id_canvas, id_c4.toString());
+  await trash(id_canvas,id_cabecalho, id_c4.toString());
+  await titulo(id_fonte, id_fundo, id_titulo,id_canvas, id_cabecalho, id_c4.toString());
   await br(id_canvas, id_c4.toString());
   await texto1(id_fonte, id_fundo, id_canvas, areatexto, id_c4.toString());
   await limpar(id_titulo, id_textarea, id_fonte, id_fundo);
   id_c4= somaID(id_c4);
+  //Realiza a comunição com as variáveis definidas após o click com as funções de criação das divs, e manter contadores para cada um dos canvas
 }
+//Gera as divs da quarta canvas, utiliza mudança do valor da variáveis a cada clique
--- js/drag&drop.js
+++ js/drag&drop.js
@@ -1,30 +1,48 @@
-var dragged;
+var dragged;//Utilizada para armazenar os elementos clicados que serão arrastados
+
+//Usado quando o elemento está sendo arrastado
 document.addEventListener("drag", function dragstart(event){
 }, false);
+//Usado quando o elemento está sendo arrastado
+
+//Acionado quando um usuário tenta arrastar um elemento inválido
 document.addEventListener("dragstart", function(event){
-  dragged1 = event.target;
+  dragged = event.target;
 }, false);
+//Acionado quando um usuário tenta arrastar um elemento inválido
+
+//Utilizada quando a operação de drag está sendo finalizada
 document.addEventListener("dragend", function(event){
   event.target.style.opacity = "";
 }, false);
+//Utilizada quando a operação de drag está sendo finalizada
+
+//Acionado quando o elemento arrastável está em cima de um ponto de soltura válido
 document.addEventListener("dragover", function(event){
   event.preventDefault();
 }, false);
+//Acionado quando o elemento arrastável está em cima de um ponto de soltura válido
+
+//Funciona quando o elemento arrastável entra em um ponto de soltura
 document.addEventListener("dragenter", function(event){
   if(event.target.className == "mid"){
-    event.target.style.backgroundColor = "red";
   }
 }, false);
+//Funciona quando o elemento arrastável entra em um ponto de soltura
+
+//Aciona quando o elemento abandona o ponto de soltura
 document.addEventListener("dragleave", function(event){
   if(event.target.className == "mid"){
-    event.target.style.backgroundColor = "";
   }
 }, false);
+//Aciona quando o elemento abandona o ponto de soltura
+
+//Determina de onde o elemento será removido e onde será adicionado
 document.addEventListener("drop", function(event){
   event.preventDefault();
   if(event.target.className == "mid"){
-    event.target.style.backgroundColor = "";
     dragged.parentNode.removeChild(dragged);
     event.target.appendChild(dragged);
   }
 }, false);
+//Determina de onde o elemento será removido e onde será adicionado
